4be4f98abd3d085c9772d7860800949b9b02e3ca
Philippe Assis <codephilippe@gmail.com>
2024-04-14 18:50:55 -0300

    0.13.1

--- src/controllers/loader.ts
+++ src/controllers/loader.ts
@@ -4,11 +4,13 @@ import * as path from 'path';
 // Load all controllers from the controllers directory by root path project
 export function loadControllers(projectRootPath: string) {
   const controllersPath = path.join(projectRootPath, 'src/controllers');
+  const names: string[] = [];
 
-  const controllers = fs.readdirSync(controllersPath).map((file) => {
+  fs.readdirSync(controllersPath).map((file) => {
     const controllerPath = path.join(controllersPath, file);
+    names.push(controllerPath);
     return require(controllerPath);
   });
 
-  return controllers;
+  return names;
 }
